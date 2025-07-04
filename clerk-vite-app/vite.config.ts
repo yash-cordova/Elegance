4cbb03980e4c9d4a3ebb878d30f00bdd6038f59e
Yash <iyash@Yash.com>
2025-07-04 21:43:08 +0530

    first commit

--- clerk-vite-app/vite.config.ts
+++ clerk-vite-app/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // 👈 Set to '/' for Netlify root deployments
   plugins: [react()],
 })
